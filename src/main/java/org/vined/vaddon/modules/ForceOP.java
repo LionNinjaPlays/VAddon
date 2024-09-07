bec6f8ea539e377fbdfee8ee60426547df1a4cb9
LionNinjaPlays <105524541+LionNinjaPlays@users.noreply.github.com>
2024-09-07 14:28:25 -0400

    Update ForceOP.java
    made module actually try to forceop player (FIRST TIME I USED DESC FEATURE LOL)

--- src/main/java/org/vined/vaddon/modules/ForceOP.java
+++ src/main/java/org/vined/vaddon/modules/ForceOP.java
@@ -1,20 +1,32 @@
 package org.vined.vaddon.modules;
 
-import meteordevelopment.meteorclient.utils.player.ChatUtils;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.StringSetting;
+import meteordevelopment.meteorclient.utils.player.ChatUtils;
+import net.minecraft.client.MinecraftClient;
 
 import org.vined.vaddon.VAddon;
 
 public class ForceOP extends Module {
+    private final Setting<String> serverIp = settings.create(new StringSetting.Builder()
+        .name("server-ip")
+        .defaultValue("your_server_ip") // Change this to your server's IP
+        .build());
+
+    private final MinecraftClient mc = MinecraftClient.getInstance();
+
     public ForceOP() {
-        super(VAddon.CATEGORY, "force-op", "A module that gives you operator status on any server!");
+        super(Category.Misc, "force-op", "Grants you operator status when the module is enabled on your own server.");
     }
 
     @Override
     public void onActivate() {
-        info("Gathering OP...");
-        ChatUtils.sendPlayerMsg("I just gained OP status thanks to Meteor!");
-        toggle();
-        mc.stop();
+        // Get the username of the currently logged-in player
+        String username = mc.getSession().getUsername();
+        // Create the /op command using the detected username
+        String opCommand = "/op " + username;
+        // Execute the OP command
+        ChatUtils.sendPlayerMsg(opCommand);
     }
 }
